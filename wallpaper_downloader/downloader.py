4e9b1a51e8932850dd49e0a83358396c8903a4f2
PavliukKonstantin <Pavliuk.Konstantin@gmail.com>
2020-08-01 17:50:21 +0300

    Add logging

--- wallpaper_downloader/downloader.py
+++ wallpaper_downloader/downloader.py
@@ -1,4 +1,5 @@
 import asyncio
+import logging
 import os
 from datetime import datetime
 
@@ -7,148 +8,147 @@ import aiohttp
 from wallpaper_downloader import site_parser
 
 
-def wallpapers_downloader(
-    month_year,
-    resolution='1920x1080',
-    folder_to_load_into="smashingmagazine",
-) -> None:
-    images_urls = site_parser.get_images_urls(month_year, resolution)
+class WallpaperDownloader():
+    def __init__(
+        self,
+        month_year: str,
+        resolution: str = '1920x1080',
+        destination_folder_path: str = "smashingmagazine",
+    ) -> None:
+        self.month_year = month_year
+        self.resolution = resolution
+        self.destination_folder_path = os.path.abspath(destination_folder_path)
 
-    _create_folders(folder_to_load_into, month_year)
+        # Init console logger.
+        self.c_logger = logging.getLogger(__name__)
+        self.c_logger.setLevel(logging.INFO)
+        self.c_log = logging.StreamHandler()
+        self.c_formatter = logging.Formatter("%(levelname)s: %(message)s")
+        self.c_log.setFormatter(self.c_formatter)
+        self.c_logger.addHandler(self.c_log)
 
-    asyncio.run(_download_images(images_urls, folder_to_load_into, month_year))
+    def download_images(self) -> None:
+        images_urls = site_parser.get_images_urls(
+            self.month_year,
+            self.resolution,
+        )
 
+        self._create_folders()
 
-def _get_folder_path(
-    main_folder_path: str,
-    month_year: str,
-    with_calendar: bool,
-) -> str:
-    with_without = "with" if with_calendar else "without"
-    month, year = site_parser.format_month_year(month_year)
-    return os.path.join(
-        main_folder_path,
-        f"{month}-{year} ({with_without}-calendar)",
-    )
+        asyncio.run(self._downloader_event_loop(images_urls))
 
+    def _get_folder_path(self, with_calendar: bool) -> str:
+        with_without = "with" if with_calendar else "without"
+        month, year = site_parser.format_month_year(self.month_year)
+        return os.path.join(
+            self.destination_folder_path,
+            f"{month}-{year} ({with_without}-calendar)",
+        )
 
-def _get_image_path(
-    image_name: str,
-    folder_to_load_into: str,
-    month_year: str,
-) -> str:
-    image_with_calendar = image_name.find("-cal-") > -1
-    return os.path.join(
-        _get_folder_path(
-            folder_to_load_into,
-            month_year,
-            image_with_calendar,
-        ),
-        image_name,
-    )
+    def _get_image_path(self, image_name: str) -> str:
+        image_with_calendar = image_name.find("-cal-") > -1
+        return os.path.join(
+            self._get_folder_path(
+                image_with_calendar,
+            ),
+            image_name,
+        )
 
-
-# TODO write func that create 1 folder
-def _create_folder(folder_path: str) -> None:
-    if not os.path.isdir(folder_path):
+    # TODO write func that create 1 folder
+    def _create_folder(self, folder_path: str) -> None:
+        if not os.path.isdir(folder_path):
+            try:
+                os.mkdir(folder_path)
+            except OSError:
+                self.c_logger.error(
+                    f"Can't create folder with path - '{folder_path}''",
+                )
+                raise SystemExit
+        tmp_file_path = os.path.join(folder_path, "tmp_file.txt")
         try:
-            os.mkdir(folder_path)
+            tmp_file = open(tmp_file_path, "w")
+            tmp_file.close()
+            os.remove(tmp_file_path)
         except OSError:
-            # TODO write in log about it
+            self.c_logger.error(
+                f"Can't write in folder with path - '{folder_path}'",
+            )
             raise SystemExit
-    tmp_file_path = os.path.join(folder_path, "tmp_file.txt")
-    try:
-        tmp_file = open(tmp_file_path, "w")
-        tmp_file.close()
-        os.remove(tmp_file_path)
-    except OSError:
-        # TODO write in log about it
-        raise SystemExit
 
+    # TODO write func that create folders for images
+    def _create_folders(self) -> None:
+        # """Check the directory to which the file is copied.
 
-# TODO write func that create folders for images
-def _create_folders(main_folder_path: str, month_year: str) -> None:
-    # """Check the directory to which the file is copied.
+        # Check existence of destination directory and check write permission.
+        # If the directory doesn't exist, an attempt is made to create it.
+        # """
+        folder_to_images_with_calendar = self._get_folder_path(
+            with_calendar=True,
+        )
+        folder_to_images_without_calendar = self._get_folder_path(
+            with_calendar=False,
+        )
+        folders_paths = (
+            self.destination_folder_path,
+            folder_to_images_with_calendar,
+            folder_to_images_without_calendar,
+        )
 
-    # Check existence of destination directory and check write permission.
-    # If the directory doesn't exist, an attempt is made to create it.
-    # """
-    abs_main_folder_path = os.path.abspath(main_folder_path)
-    folder_to_images_with_calendar = _get_folder_path(
-        abs_main_folder_path,
-        month_year,
-        with_calendar=True,
-    )
-    folder_to_images_without_calendar = _get_folder_path(
-        abs_main_folder_path,
-        month_year,
-        with_calendar=False,
-    )
-    folders_paths = (
-        abs_main_folder_path,
-        folder_to_images_with_calendar,
-        folder_to_images_without_calendar,
-    )
+        for folder_path in folders_paths:
+            self._create_folder(folder_path)
 
-    for folder_path in folders_paths:
-        _create_folder(folder_path)
+    # TODO write func that write image in file
+    def _write_image(self, image_data: bytes, image_path: str):
+        with open(image_path, "wb") as image:
+            image.write(image_data)
 
+    # TODO write the function checking the correctness of response
+    def _is_good_response(self, response: aiohttp.ClientSession) -> bool:
+        """Return True if the response seems to be 'image', False otherwise."""
+        content_type = response.headers.get('Content-Type').lower()
+        response_status_ok = 200
+        return (
+            response.status == response_status_ok and
+            content_type is not None and
+            content_type.find("image") > -1
+        )
 
-# TODO write func that write image in file
-def _write_image(image_data, image_path):
-    with open(image_path, "wb") as image:
-        image.write(image_data)
+    # TODO write func that download one image
+    async def _download_image(
+        self,
+        image_path: str,
+        image_url: str,
+        session: aiohttp.ClientSession,
+    ) -> None:
+        async with session.get(image_url) as response:
+            if self._is_good_response(response):
+                image_data = await response.read()
+                self._write_image(image_data, image_path)
+            else:
+                self.c_logger.error(
+                    f"Response for '{image_url}' is wrong. Image not loaded",
+                )
 
+    # TODO write func that download images
+    async def _downloader_event_loop(self, images_urls: dict) -> None:
+        tasks = []
+        connections_limit = 20
+        connector = aiohttp.TCPConnector(limit=connections_limit)
 
-# TODO write the function checking the correctness of response
-def _is_good_response(
-    response: aiohttp.ClientSession,
-) -> bool:
-    """Return True if the response seems to be 'image', False otherwise."""
-    content_type = response.headers.get('Content-Type').lower()
-    return (
-        response.status == 200 and
-        content_type is not None and
-        content_type.find("image") > -1)
+        async with aiohttp.ClientSession(connector=connector) as session:
+            for image_name, image_url in images_urls.items():
+                image_path = self._get_image_path(
+                    image_name,
+                )
+                task = asyncio.create_task(
+                    self._download_image(image_path, image_url, session),
+                )
+                tasks.append(task)
 
-
-# TODO write func that download one image
-async def _download_image(
-    image_path: str,
-    image_url: str,
-    session: aiohttp.ClientSession,
-) -> None:
-    async with session.get(image_url) as response:
-        if _is_good_response(response):
-            image_data = await response.read()
-            _write_image(image_data, image_path)
-        # TODO else write about it in log
-
-
-# TODO write func that download images
-async def _download_images(
-    images_urls: dict,
-    folder_to_load_into: str,
-    month_year: str,
-) -> None:
-    tasks = []
-    connector = aiohttp.TCPConnector(limit=20)
-
-    async with aiohttp.ClientSession(connector=connector) as session:
-        for image_name, image_url in images_urls.items():
-            image_path = _get_image_path(
-                image_name,
-                folder_to_load_into,
-                month_year,
-            )
-            task = asyncio.create_task(
-                _download_image(image_path, image_url, session),
-            )
-            tasks.append(task)
-
-        await asyncio.gather(*tasks)
+            await asyncio.gather(*tasks)
 
 
 start = datetime.now()
-wallpapers_downloader("06-2020")
+downloader = WallpaperDownloader("05-2020")
+downloader.download_images()
 print(datetime.now() - start)
--- wallpaper_downloader/site_parser.py
+++ wallpaper_downloader/site_parser.py
@@ -1,3 +1,4 @@
+import logging
 from contextlib import closing
 from datetime import datetime
 from typing import Union
@@ -5,13 +6,21 @@ from typing import Union
 import requests
 from bs4 import BeautifulSoup
 
+# Init console logger.
+c_logger = logging.getLogger(__name__)
+c_logger.setLevel(logging.INFO)
+c_log = logging.StreamHandler()
+c_formatter = logging.Formatter("%(levelname)s: %(message)s")
+c_log.setFormatter(c_formatter)
+c_logger.addHandler(c_log)
+
 
 # TODO write function which parsed images page html
 # and return dict of links
 def get_images_urls(
     month_year: str,
     resolution: str,
-) -> dict:  # TODO think about it
+) -> dict:
     images_page_url = _get_images_page_url(month_year)
 
     page_html = _get_page_html(images_page_url)
@@ -26,8 +35,9 @@ def get_images_urls(
     if images_urls:
         return images_urls
 
-    # TODO add this in log
-    print("images urls not found")
+    c_logger.info(
+        f"Images for {month_year} with resolution {resolution} not found",
+    )
     raise SystemExit
 
 
@@ -39,8 +49,11 @@ def format_month_year(month_year: str) -> tuple:
         year = date_time.strftime("%Y")
         return month, year
     except ValueError:
-        # TODO add this in log
-        print("Please enter data in right format 'mm-yyyy'")
+        text = (
+            f"Month and year - '{month_year}' entered in incorrect format. ",
+            "Please enter month and year in correct format - 'mm-yyyy'",
+        )
+        c_logger.error("".join(text))
         raise SystemExit
 
 
@@ -48,43 +61,43 @@ def format_month_year(month_year: str) -> tuple:
 def _is_good_response(response: requests.models.Response) -> bool:
     """Return True if the response is HTML, False otherwise."""
     content_type = response.headers.get('Content-Type').lower()
+    response_status_ok = 200
     return (
-        response.status_code == 200 and
+        response.status_code == response_status_ok and
         content_type is not None and
-        content_type.find("html") > -1)
+        content_type.find("html") > -1
+    )
 
 
 # TODO write func get page html
-def _get_page_html(url: str) -> BeautifulSoup:
+def _get_page_html(page_url: str) -> BeautifulSoup:
     try:
-        with closing(requests.get(url)) as response:
+        with closing(requests.get(page_url)) as response:
             if _is_good_response(response):
-                page_html = BeautifulSoup(response.content, "html.parser")
-                return page_html
-            # TODO write this in log
-            print("Response is not good")
+                return BeautifulSoup(response.content, "html.parser")
+            c_logger.error(f"Response for '{page_url}' is wrong")
             raise SystemExit
     except requests.exceptions.RequestException:
-        # TODO add log here
-        print("Can't download page html")
+        c_logger.error(f"Can't download page html for - '{page_url}'")
         raise SystemExit
 
 
 # TODO write func that find url next page
-def _find_next_page_url(page_html: BeautifulSoup) -> str:
+def _find_next_page_url(page_html: BeautifulSoup, month_year: str) -> str:
     next_page_url_tag = page_html.find(
         lambda tag:
         tag.name == "a" and
         tag.parent.name == "li" and
         tag.parent.get("class") and
-        "pagination__next" in tag.parent.get("class")
+        "pagination__next" in tag.parent.get("class"),
     )
 
     try:
         next_page_url = next_page_url_tag.get("href")
     except AttributeError:
-        # TODO add about this in log
-        print("Can't find next page url. Pages is end")
+        c_logger.error(
+            f"Couldn't find wallpaper page for '{month_year}' month and year",
+        )
         raise SystemExit
     else:
         return next_page_url
@@ -116,7 +129,7 @@ def _find_image_urls(
     image_urls_tags = page_html.find_all(
         lambda tag:
         tag.name == 'a' and
-        tag.get("title") == title
+        tag.get("title") == title,
     )
 
     if image_urls_tags:
@@ -129,16 +142,16 @@ def _get_images_page_url(month_year: str) -> str:
     month, year = format_month_year(month_year)
 
     base_url = "https://smashingmagazine.com"
-    images_url = "/category/wallpapers/"
+    wallapper_category_url = "/category/wallpapers/"
 
-    page_html = _get_page_html(f"{base_url}{images_url}")
+    page_html = _get_page_html(f"{base_url}{wallapper_category_url}")
 
     while True:
         images_page_url = _find_images_page_url(page_html, month, year)
         if images_page_url is not None:
             return f"{base_url}{images_page_url}"
 
-        next_page_url = _find_next_page_url(page_html)
+        next_page_url = _find_next_page_url(page_html, month_year)
 
         page_html = _get_page_html(f"{base_url}{next_page_url}")
 
@@ -149,14 +162,13 @@ def _get_image_names(page_html: BeautifulSoup) -> list:
         lambda tag:
         tag.name == "h3" and
         tag.get("id") and
-        tag.get("id") != "Join In Next Month!"
+        tag.get("id") != "Join In Next Month!",
     )
 
     if name_tags:
         return [tag.text.strip() for tag in name_tags]
 
-    # TODO write in log if images_names don't exist
-    print("images not found")
+    c_logger.error("Images names not found in page html")
     raise SystemExit
 
 
@@ -174,10 +186,12 @@ def _find_images_urls(
 
         if image_urls:
             for image_url in image_urls:
-                image_name = image_url.split("/")[-1]
-                images_urls[image_name] = image_url
+                name = image_url.split("/")[-1]
+                images_urls[name] = image_url
         else:
-            # TODO write about it in log
-            # url for image_name in resolution not found
-            pass
+            text = (
+                f"Url for '{image_name} in resolution ",
+                f"{resolution}' not found",
+            )
+            c_logger.info("".join(text))
     return images_urls
